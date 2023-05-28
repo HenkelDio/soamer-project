27c0f36aaeac910de477addf8dc6e9e90147357c
HenkelDio <willianhenkel@gmail.com>
2023-05-28 20:26:55 -0300

    Updated estoque page

--- client/src/pages/layout/estoque/index.js
+++ client/src/pages/layout/estoque/index.js
@@ -1,8 +1,13 @@
+import { Button } from "../../../components/Button";
 import Content from "../../../components/content";
 import Header from "../../../components/header";
 import Menu from "../../../components/menu";
 import SideMenu from "../../../components/side_menu";
 import { Container } from "../style";
+import { Card } from "./style";
+import { FaEdit } from "react-icons/fa"
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 
 export default function Estoque() {
   return(
@@ -11,67 +16,13 @@ export default function Estoque() {
       <Container>
         <SideMenu />
         <Content>
-          <h2>Estoque</h2>
-          <button>Novo cadastro</button>
-          <div>
-            <table>
-              <tr>
-                <td>Cód. Produto</td>
-                <td>Descrição</td>
-              </tr>
-              <tr>
-                <td>SO-37101</td>
-                <td>PONT INOX RED UNIVERSAL Ø 101,6mm COMPRIMENTO 125mm</td>
-              </tr>
-              <tr>
-                <td>SO-A01C</td>
-                <td>PONT INOX CHANF AUDI A1</td>
-              </tr>
-              <tr>
-                <td>SO-A02C</td>
-                <td>PONT INOX CHANF AUDI A3 HATCH OU SEDAN</td>
-              </tr>
-              <tr>
-                <td>SO-A03A</td>
-                <td>PONT INOX CHANF AUDI A4 AVANT ATÉ 2016</td>
-              </tr>
-            </table>
-          </div>
-          <div>
-            <h3>Descrição detalhada</h3>
-            <table>
-              <tr>
-                <td>Cód. Produto</td>
-                <td>Descrição</td>
-              </tr>
-              <tr>
-                <td>SO-37101</td>
-                <td>PONT INOX RED UNIVERSAL Ø 101,6mm COMPRIMENTO 125mm</td>
-              </tr>
-            </table>
-            <table>
-            <tr>
-              <td>Modelo</td>
-                <td>Industria</td>
-                <td>Preço</td>
-                <td>Peso Bruto (Kg)</td>
-                <td>Estoque</td>
-                <td>Estoque Mín</td>
-                <td>Local Estoque</td>
-                <td>Info</td>
-              </tr>
-              <tr>
-                <td>BOCAL REDONDO Ø 101,6mm</td>
-                <td>SOAMER PONTEIRAS AUTOMOTIVAS</td>
-                <td>R$ 222,87</td>
-                <td>0,64</td>
-                <td>0</td>
-                <td>0</td>
-                <td>-</td>
-                <td>CHANFRADA</td>
-              </tr>
-            </table>
-          </div>
+          <Button>Novo cadastro</Button>
+          <Card>
+            <p><strong>Cód. produto: </strong>8084450</p>
+            <p><strong>Descrição:</strong> PONT INOX RED UNIVERSAL Ø 101,6mm COMPRIMENTO 125mm</p>
+            <Link to='/'><FaEdit/></Link>
+            <Link>ver mais</Link>
+          </Card>
         </Content>
         <Menu />
       </Container>
