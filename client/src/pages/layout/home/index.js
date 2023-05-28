34e2ad83a6567c2f7fe7fb761dad3e8ae5b8825d
Willian Henkel <willian.deus@hitechnologies.com.br>
2023-05-28 20:27:22 -0300

    Created home page screen.

--- client/src/pages/layout/home/index.js
+++ client/src/pages/layout/home/index.js
@@ -3,6 +3,7 @@ import Header from "../../../components/header";
 import Menu from "../../../components/menu";
 import SideMenu from "../../../components/side_menu";
 import { Container } from "../style";
+import { CardsContainer, Card, MajorCard } from "./style";
 
 export default function Home() {
   return(
@@ -11,7 +12,13 @@ export default function Home() {
     <Container>
       <SideMenu />
       <Content>
-        <h2>Home</h2>
+        <CardsContainer>
+          <Card></Card>
+          <Card></Card>
+          <MajorCard></MajorCard>
+          <Card></Card>
+          <Card></Card>
+        </CardsContainer>
       </Content>
       <Menu />
     </Container>
---
+++ client/src/pages/layout/home/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const CardsContainer = styled.div`
+  width: 772px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const Card = styled.div`
+  width: calc(400px - 20px);
+  height: 250px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+`;
+
+export const MajorCard = styled.div`
+  width: 100%;
+  height: 250px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+`;
