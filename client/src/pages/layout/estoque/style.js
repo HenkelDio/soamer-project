236202ea424825995ae93d69de5e87fc51b93c2a
HenkelDio <willianhenkel@gmail.com>
2023-05-28 21:40:51 -0300

    Updated estoque page

--- client/src/pages/layout/estoque/style.js
+++ client/src/pages/layout/estoque/style.js
@@ -1,8 +1,44 @@
 import styled from "styled-components";
 
-export const Card = styled.div`
-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-border-radius: 6px;
-padding: 15px;
+export const ContainerEstoque = styled.div`
+  display: flex;
+  flex-direction:column;
+  gap: 15px;
 
+  input{
+    border: none;
+    background-color: #E0E0E0;
+    border-radius: 6px;
+    padding: 8px 10px;
+  }
 `;
+
+
+export const CardContainer = styled.div`
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 6px;
+  padding: 15px;
+  font-family: 'Inter';
+  margin-top: 10px;
+`;
+
+export const ContainerOptions = styled.div`
+  margin-top: 10px;
+  display: flex;
+  align-items: start;
+  gap: 10px;
+
+  a{
+    color: black;
+    text-decoration: none;
+  }
+
+  .btn{
+    font-size: 15px;
+  }
+  .btnEdit{
+    font-size: 18px;
+
+  }
+`;
+
--- client/src/styles/Global.js
+++ client/src/styles/Global.js
@@ -9,5 +9,26 @@ export default createGlobalStyle`
 
   body {
     background-color: #FAFAFA;
+
+    /* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 10px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
   }
 `;
