0e3ab5f7fd3493ebbcc83a46e2ee3c8e1fbeea2f
HenkelDio <willianhenkel@gmail.com>
2023-05-28 21:40:51 -0300

    Updated estoque page

---
+++ client/src/pages/layout/estoque/Card.js
@@ -0,0 +1,16 @@
+import { CardContainer,ContainerOptions } from "./style";
+import { FaEdit } from "react-icons/fa"
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+export default function Card(props){
+  return(
+    <CardContainer>
+    <p><strong>Cód. produto: </strong> {props.code}</p>
+    <p><strong>Descrição:</strong> {props.description}</p>
+    <ContainerOptions>
+    <Link to='/' className='btnEdit'><FaEdit/></Link>
+    <Link to='/' className='btn'>ver mais</Link>
+    </ContainerOptions>
+  </CardContainer>
+  )
+}
--- client/src/pages/layout/estoque/index.js
+++ client/src/pages/layout/estoque/index.js
@@ -1,28 +1,38 @@
+import { useState } from "react";
 import { Button } from "../../../components/Button";
 import Content from "../../../components/content";
 import Header from "../../../components/header";
 import Menu from "../../../components/menu";
 import SideMenu from "../../../components/side_menu";
 import { Container } from "../style";
-import { Card } from "./style";
-import { FaEdit } from "react-icons/fa"
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
-
+import Card from "./Card";
+import { ContainerEstoque } from "./style";
 
 export default function Estoque() {
+  const [data, setData] = useState([
+    {code:  'SO-37101', description: 'PONT INOX RED UNIVERSAL Ø 101,6mm COMPRIMENTO 125mm'}, 
+    {code:  'SO-A01C', description: 'PONT INOX CHANF AUDI A1'},
+    {code:  'SO-A02C', description: 'PONT INOX CHANF AUDI A3 HATCH OU SEDAN'}
+  ])
+
   return(
     <>
       <Header />
       <Container>
         <SideMenu />
         <Content>
+          <ContainerEstoque>
+          <input type="text" placeholder="Pesquisar" />
           <Button>Novo cadastro</Button>
-          <Card>
-            <p><strong>Cód. produto: </strong>8084450</p>
-            <p><strong>Descrição:</strong> PONT INOX RED UNIVERSAL Ø 101,6mm COMPRIMENTO 125mm</p>
-            <Link to='/'><FaEdit/></Link>
-            <Link>ver mais</Link>
-          </Card>
+          {
+            data.map((data) => {
+              return <Card
+              code={data.code}
+              description={data.description}
+              />
+            })
+          }
+          </ContainerEstoque>
         </Content>
         <Menu />
       </Container>
