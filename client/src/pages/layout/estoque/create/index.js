1dbd9016e5898740677f8e8beafce207c9224ed3
AlexiaBernacki <abernacki32@gmail.com>
2023-06-03 22:48:57 -0300

    att

--- client/src/pages/layout/estoque/create/index.js
+++ client/src/pages/layout/estoque/create/index.js
@@ -8,8 +8,72 @@ import { ContainerCreateEstoque } from "./style";
 import Input from "../../../../components/Input";
 import Select from "../../../../components/Select";
 import Button from "../../../../components/login/button";
+import { useEffect, useState } from "react";
 
 export default function EstoqueCreate() {
+
+    const [name, setName] = useState('');
+    const [codigo, setCodigo] = useState('');
+    const [modelo, setModelo] = useState('');
+    const [preco, setPreco] = useState('');
+    const [precoBruto, setPrecoBruto] = useState('');
+    const [quantidade, setQuantidade] = useState('');
+    const [estoqueMinimo, setEstoqueMinimo] = useState('');
+
+  useEffect(() => {
+    function handleLocalStorage(){
+      localStorage.setItem('nome','alexia')
+    }
+    handleLocalStorage();
+  }, [])
+
+  function handleChangeName(e){
+    setName(e.target.value)
+  }
+
+  function handleChangeCodigo(e){
+    setCodigo(e.target.value)
+  }
+
+  function handleChangeModelo(e){
+    setModelo(e.target.value)
+  }
+
+  function handleChangePreco(e){
+    setPreco(e.target.value)
+  }
+
+  function handleChangePrecoBruto(e){
+    setPrecoBruto(e.target.value)
+  }
+
+  function handleChangeQuantidade(e){
+    setQuantidade(e.target.value)
+  }
+
+  function handleChangeEstoqueMinimo(e){
+    setEstoqueMinimo(e.target.value)
+  }
+
+  function handleShowInformation(e){
+    e.preventDefault();
+
+    const data = ({
+      name,
+      codigo,
+      modelo,
+      preco,
+      precoBruto,
+      quantidade,
+      estoqueMinimo
+    })
+
+    const code = JSON.stringify(data.codigo);
+    const toString = JSON.stringify(data);
+
+    localStorage.setItem('item', toString)
+  }
+
   return(
     <>
       <Header />
@@ -20,20 +84,38 @@ export default function EstoqueCreate() {
           <ContainerCreateEstoque>
             <Link to="/estoque">{'<'} Voltar</Link>
             <h2>Cadastrar novo item</h2>
-            <form>
-              <Input placeholder="Código"/>
-              <Input placeholder="Nome"/>
-              <Input placeholder="Modelo"/>
+            <form onSubmit={e => handleShowInformation(e)}>
+              <Input
+                onChange={e => handleChangeCodigo(e)}
+                placeholder="Código"/>
+              <Input 
+                onChange={e => handleChangeName(e)}
+                placeholder="Nome"/>
+              <Input
+                onChange={e => handleChangeModelo(e)}
+                placeholder="Modelo"/>
               <Select>
-                <option value='' disabled selected hidden>Indústria</option>
+                <option disabled hidden>Indústria</option>
                 <option>SOAMER PONTEIRAS AUTOMOTIVAS</option>
                 <option>GM CONCESSIONÁRIAS</option>
               </Select>
               <div>
-                <Input placeholder="Preço" type="number"/>
-                <Input placeholder="Peso bruto (Kg)" type="number" />
-                <Input placeholder="Quantidade" type="number"/> 
-                <Input placeholder="Estoque mínimo" type="number"/>
+                <Input
+                  onChange={e => handleChangePreco(e)}
+                  placeholder="Preço" 
+                  type="number"/>
+                <Input
+                  onChange={e => handleChangePrecoBruto(e)}
+                  placeholder="Peso bruto (Kg)"
+                  type="number" />
+                <Input 
+                  onChange={e => handleChangeQuantidade(e)}
+                  placeholder="Quantidade" 
+                  type="number"/> 
+                <Input
+                onChange={e => handleChangeEstoqueMinimo(e)}
+                placeholder="Estoque mínimo" 
+                type="number"/>
               </div>
               <Button>Salvar</Button>
             </form>
